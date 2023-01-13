1f8a486dd74c857e38c5d3f2a00dda67380894ac
leeuihyun <meta212121@naver.com>
2023-01-13 20:58:59 +0900

    [init] 라우트 설정

---
+++ freecard/src/components/HomeComponent.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CardComponent from "./CardComponent";
+
+function HomeComponent() {
+  return (
+    <div>
+      <CardComponent></CardComponent>
+    </div>
+  );
+}
+
+export default HomeComponent;
---
+++ freecard/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HomeComponent from "../components/HomeComponent";
+
+function Home() {
+  return (
+    <div>
+      <HomeComponent></HomeComponent>
+    </div>
+  );
+}
+
+export default Home;
